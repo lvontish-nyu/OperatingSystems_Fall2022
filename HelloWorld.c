6e52e7a8346d5ae03c398f19f1c045f5d83d233e
lvontish-nyu <91288146+lvontish-nyu@users.noreply.github.com>
2022-10-15 15:26:00 -0500

    Create HelloWorld.c
    Created Homework File

---
+++ HelloWorld.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+	HelloWorld.c
+	Lindsay Von Tish
+	Operating Systems
+	Fall 2022
+	lmv9443@nyu.edu
+*/
+
+int main(int argc, char *argv[]) {
+	printf("Hello World!");
+	return(0);
+}
