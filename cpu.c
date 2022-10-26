de48eef66358bc3a1db4bbfc124555c422510af8
lvontish-nyu <91288146+lvontish-nyu@users.noreply.github.com>
2022-10-26 18:38:46 -0500

    Update cpu.c

--- cpu.c
+++ cpu.c
@@ -162,6 +162,7 @@ struct PCB handle_process_arrival_pp(struct PCB ready_queue[QUEUEMAX], int *queu
 		*/
 		new_process.execution_starttime = 0;
 		new_process.execution_endtime = 0;
+		new_process.remaining_bursttime = new_process.total_bursttime;
 		ready_queue[*queue_cnt] = new_process;
 		++*queue_cnt;
 
