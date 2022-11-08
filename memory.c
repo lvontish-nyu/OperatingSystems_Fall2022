379efefc72c7b881af82108aff7355c3bf7300d5
lvontish-nyu <91288146+lvontish-nyu@users.noreply.github.com>
2022-11-08 17:38:50 -0600

    Start new function

--- memory.c
+++ memory.c
@@ -10,12 +10,138 @@
 	lmv9443@nyu.edu
 */
 
+/*
+	struct MEMORY_BLOCK {
+		int start_address;
+		int end_address;
+		int segment_size;
+		int process_id; //0 indicates a free block
+	}
+*/
+
 /* Global Variables */
+struct MEMORY_BLOCK NULLBLOCK = {.start_address = 0, .end_address = 0, .segment_size = 0, .process_id = 0};
 
 /* Function Signatures */
+
+/* My Functions */
+/* print_BLOCK */
+int print_BLOCK(struct MEMORY_BLOCK b);
+/* print_MAP - Prints memory map*/
+int print_MAP(struct MEMORY_BLOCK memory_map[MAPMAX], int *map_cnt);
+/* split_BLOCK */
+int split_Block(struct MEMORY_BLOCK memory_map[MAPMAX], int *map_cnt, int position, int request_size);
+
 /* Assignment Functions */
 /* best_fit_allocate */
 struct MEMORY_BLOCK best_fit_allocate(int request_size, struct MEMORY_BLOCK memory_map[MAPMAX],int *map_cnt, int process_id);
+/* first_fit_allocate */
+struct MEMORY_BLOCK first_fit_allocate(int request_size, struct MEMORY_BLOCK memory_map[MAPMAX],int *map_cnt, int process_id);
+/* worst_fit_allocate */
+struct MEMORY_BLOCK worst_fit_allocate(int request_size, struct MEMORY_BLOCK memory_map[MAPMAX],int *map_cnt, int process_id);
+/* next_fit_allocate */
+struct MEMORY_BLOCK next_fit_allocate(int request_size, struct MEMORY_BLOCK memory_map[MAPMAX],int *map_cnt, int process_id, int last_address);
+/* release_memory */
+void release_memory(struct MEMORY_BLOCK freed_block, struct MEMORY_BLOCK memory_map[MAPMAX],int *map_cnt);
+
+
+/* Main Method */
+/*
+int main(int argc, char *argv[]) {
+
+}
+*/
+
+/* My functions */
+
+/* print_BLOCK */
+int print_BLOCK(struct MEMORY_BLOCK b){
+	printf("[%d, %d, %d, %d]\n", b.start_address, b.end_address, b.segment_size, b.process_id);
+	return(0);
+}
+
+/* print_MAP - Prints memory map*/
+int print_MAP(struct MEMORY_BLOCK memory_map[MAPMAX], int *map_cnt){
+	for(int i = 0; i < *map_cnt; i++){
+		print_BLOCK(memory_map[i]);
+	}
+	return(0);
+}
+
+int split_Block(struct MEMORY_BLOCK memory_map[MAPMAX], int *map_cnt, int position, int request_size){
+	// Shift BLOCKs after the split forward
+	
+	for(int i = *map_cnt; i > position; i--){
+		memory_map[i] = memory_map[i -1];
+	}
+	printf("Blocks shifted\n");
+	// Split block at position
+	struct MEMORY_BLOCK og_BLOCK = memory_map[position];
+	memory_map[position].segment_size = request_size;
+	memory_map[position].end_address = memory_map[position].start_address + request_size - 1;
+
+	memory_map[position + 1].segment_size -= request_size;
+	memory_map[position + 1].start_address = memory_map[position].end_address + 1;
+
+	return(0);
+
+}
+
+/* Assignment Functions */
+/*
+best_fit_allocate
+	This method allocates memory according to the Best Fit scheme.
+	Inputs
+		process id of the requesting process
+		size of the memory being requested
+		the memory map
+	It finds the candidate memory blocks that can be allocated and chooses the one whose size is closest to the requested size.
+*/
+struct MEMORY_BLOCK best_fit_allocate(int request_size, struct MEMORY_BLOCK memory_map[MAPMAX],int *map_cnt, int process_id){
+	// Find memory block
+	int sizeDiff;
+	int bestDiff = memory_map[0].segment_size - request_size;
+	int position = 0;
+
+	for(int i = 0; i < *map_cnt; i++){
+		sizeDiff = memory_map[i].segment_size - request_size;
+		// Only care about free blocks w/ a PID of 0 with enough memory to fit the new block
+		if(memory_map[i].process_id == 0 && sizeDiff >= 0){
+			if(sizeDiff == 0){
+				/*
+					If the free block found is exactly of the same size as the requested size
+						the method updates the process id to allocate it
+						returns this memory block.
+					Can return first exact fit memory block
+				*/
+				memory_map[i].process_id = process_id;
+				return(memory_map[i]);
+			}else if(sizeDiff < bestDiff && bestDiff >= 0){
+				position = i;
+				bestDiff = sizeDiff;
+			}
+		}
+	}
+
+
+	if(bestDiff < 0){
+		// if there is no free block of memory (in the memory map) that is at least as large as the requested size, the method returns the NULLBLOCK.
+		return(NULLBLOCK);
+	}else{
+		/*
+			If the free block found is larger than the requested size
+			the block is split into two pieces
+				the first piece allocated
+				the second piece becoming a free block in the memory map
+		*/
+		
+		split_Block(memory_map, map_cnt, request_size, position);
+		memory_map[position].process_id = process_id;
+
+	}
+}
+
+
 /* first_fit_allocate */
 struct MEMORY_BLOCK first_fit_allocate(int request_size, struct MEMORY_BLOCK memory_map[MAPMAX],int *map_cnt, int process_id);
 /* worst_fit_allocate */
