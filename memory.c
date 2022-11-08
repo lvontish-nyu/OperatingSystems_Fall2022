4bc9f111314bc225d24f8aa670ed4e3615b52056
lvontish-nyu <91288146+lvontish-nyu@users.noreply.github.com>
2022-11-08 17:33:18 -0600

    yeet

--- memory.c
+++ memory.c
@@ -9,3 +9,18 @@
 	Fall 2022
 	lmv9443@nyu.edu
 */
+
+/* Global Variables */
+
+/* Function Signatures */
+/* Assignment Functions */
+/* best_fit_allocate */
+struct MEMORY_BLOCK best_fit_allocate(int request_size, struct MEMORY_BLOCK memory_map[MAPMAX],int *map_cnt, int process_id);
+/* first_fit_allocate */
+struct MEMORY_BLOCK first_fit_allocate(int request_size, struct MEMORY_BLOCK memory_map[MAPMAX],int *map_cnt, int process_id);
+/* worst_fit_allocate */
+struct MEMORY_BLOCK worst_fit_allocate(int request_size, struct MEMORY_BLOCK memory_map[MAPMAX],int *map_cnt, int process_id);
+/* next_fit_allocate */
+struct MEMORY_BLOCK next_fit_allocate(int request_size, struct MEMORY_BLOCK memory_map[MAPMAX],int *map_cnt, int process_id, int last_address);
+/* release_memory */
+void release_memory(struct MEMORY_BLOCK freed_block, struct MEMORY_BLOCK memory_map[MAPMAX],int *map_cnt);
