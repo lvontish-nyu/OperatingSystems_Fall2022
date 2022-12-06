2a3995dd5551fb11192dbe855330764d22aa1d47
lvontish-nyu <91288146+lvontish-nyu@users.noreply.github.com>
2022-12-06 16:46:06 -0600

    Update virtual.c

--- virtual.c
+++ virtual.c
@@ -195,6 +195,8 @@ int count_page_faults_fifo(struct PTE page_table[TABLEMAX],int table_cnt, int re
 
 	}
 
+	return(page_faults);
+
 }
 /* process_page_access_lru */
 int process_page_access_lru(struct PTE page_table[TABLEMAX],int *table_cnt, int page_number, int frame_pool[POOLMAX],int *frame_cnt, int current_timestamp){
